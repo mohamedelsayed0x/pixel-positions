a4543f7d3f5b6730ccb9087d7d77e70d3e2a98d3
Mohamed Elsayed <moelsayko@gmail.com>
2026-08-03 23:44:04 +0300

    Style with Tailwind

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,22 +4,29 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Pixel-positions</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body>
+<body class="bg-black text-white">
     <div>
         <nav>
             <div>
                 <a href="">
-                    <img src="{{ vite::asset('resources/images/logo.svg') }}" alt="">
+                    <img src="{{ Vite::asset('resources/images/logo.svg') }}" alt="">
                 </a>
             </div>
-            <div>Links</div>
-            <div>Post a job</div>
+
+            <div>
+                <a href="#">Jobs</a>
+                <a href="#">Careers</a>
+                <a href="#">Salaries</a>
+                <a href="#">Companies</a>
+            </div>
+            <div>
+                <a href="#">Post a Job</a>
+            </div>
         </nav>
-
-
         <main>
             {{ $slot }}
         </main>
