6c68191061b1af7a591b68f4aec290f0d436abec
Mohamed Elsayed <moelsayko@gmail.com>
2026-08-08 23:49:41 +0300

    Add validations

--- app/Http/Controllers/RegisteredUserController.php
+++ app/Http/Controllers/RegisteredUserController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rules\File;
+use Illuminate\Validation\Rules\Password;
+use App\Models\User;
 
 class RegisteredUserController extends Controller
 {
@@ -27,7 +31,19 @@ class RegisteredUserController extends Controller
      */
     public function store(Request $request)
     {
-        dd($request->all());
+        $userAttributes = $request->validate([
+            'name' => ['required'],
+            'email' => ['required', 'email', 'max:254', 'unique:users,email'],
+            'password' => ['required', 'confirmed', Password::min(6)->max(16)],
+        ]);
+
+        $employerAttributes = $request->validate([
+            'name' => ['required'],
+            'logo' => ['required', File::types(['png', 'jpg', 'webp'])]
+        ]);
+
+        $user = User::create($userAttributes);
+        Auth::login($user);
     }
 
     /**
